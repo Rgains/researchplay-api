eb785d5ac0e02a1d66f0e7c1b5ac38bfcd67d274
okoisorjr <okoisorjr@gmail.com>
2026-08-19 02:33:25 +0100

    chore: lean configuration for mail transporter

--- src/utils/mailTransporter.ts
+++ src/utils/mailTransporter.ts
@@ -1,15 +1,17 @@
 import nodemailer from 'nodemailer';
 import { currentConfig } from './config';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 export const transporter = nodemailer.createTransport({
   host: currentConfig.mailHost,
   port: currentConfig.mailPort,
-  secure: true,
+  secure: false,
   auth: {
     user: currentConfig.mailUser,
     pass: currentConfig.mailPassword,
   },
+  family: 4,
   connectionTimeout: 10000,
   greetingTimeout: 10000,
   socketTimeout: 10000,
-});
+} as SMTPTransport.Options);
