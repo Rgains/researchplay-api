23fad691ad041c02bc4aec5bb264b26fa494411a
okoisorjr <okoisorjr@gmail.com>
2026-08-19 02:23:11 +0100

    chore: lean configuration for mail transporter

--- src/utils/mailTransporter.ts
+++ src/utils/mailTransporter.ts
@@ -2,9 +2,14 @@ import nodemailer from 'nodemailer';
 import { currentConfig } from './config';
 
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: currentConfig.mailHost,
+  port: currentConfig.mailPort,
+  secure: true,
   auth: {
     user: currentConfig.mailUser,
     pass: currentConfig.mailPassword,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
